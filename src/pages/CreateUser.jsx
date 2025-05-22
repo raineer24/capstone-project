b41f81b31a44e8395dea334fa335147db3e120fe
raineer <raineerdelarita@gmail.com>
2025-05-22 19:57:46 +0800

    wdiget comment

--- src/pages/CreateUser.jsx
+++ src/pages/CreateUser.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import useLocalStorageCrud from '../hooks/useLocalStorageCrud';
-import { ComplexUserForm} from '../components/ComplexUserForm';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import useLocalStorageCrud from "../hooks/useLocalStorageCrud";
+import { ComplexUserForm } from "../components/ComplexUserForm";
 
 const CreateUser = () => {
   const navigate = useNavigate();
   const { createItem } = useLocalStorageCrud();
 
   const handleCreate = (data) => {
-    console.log(data,'data!')
+    console.log(data, "data!");
     createItem(data);
-    navigate('/')
+    navigate("/");
   };
 
   return (
-    <div className='max-w-md mx-auto mt-8'>
-      <h1 className='text-2xl font-bold mb-4'>Create User</h1>
-      <ComplexUserForm onSubmit={handleCreate}/>
+    <div className="max-w-md mx-auto mt-8">
+      <h1 className="text-2xl font-bold mb-4">Create User</h1>
+      <ComplexUserForm onSubmit={handleCreate} />
     </div>
   );
 };
