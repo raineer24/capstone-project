01f426f2e6d3e927b4cb7b5f46d97f45e6478b97
raineer <raineerdelarita@gmail.com>
2025-06-02 11:52:36 +0800

    lazy loading spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,1 +1,9 @@
 import React from "react";
+
+export const Spinner = () => (
+    <div style={{ textAlign: 'center', padding: '2rem'}}>
+        <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+        </div>
+    </div>
+);
