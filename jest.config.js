5733ad37b020215dca97dc1fc383df1537ed5432
raineer <raineerdelarita@gmail.com>
2025-06-04 10:37:04 +0800

    jest config export

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
+/** @type {import('jest').Config} */
 export default {
   verbose: true,
   roots: ['<rootDir>/src'],
+  testEnvironment: 'jsdom',
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
   },
@@ -8,5 +10,7 @@ export default {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
   },
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  extensionsToTreatAsEsm: ['.jsx', '.js'],
+  resolver: undefined, // Remove custom resolver if any
 };
