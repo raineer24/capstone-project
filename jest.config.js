28930e8fda01bd732712a3851b031edbe4f77417
raineer <raineerdelarita@gmail.com>
2025-06-04 10:28:27 +0800

    jest config js

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  verbose: true,
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+};
