ab621a364b57c428f2a485f4da4b22add12d8288
raineer <raineerdelarita@gmail.com>
2025-06-10 15:20:52 +0800

    product gallery size selection

---
+++ src/components/organisms/ProductDisplay.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import ProductGallery from "./ProductGallery";
+import VariantSelector from '../molecules/VariantSelector';
+import Button from '../atoms/Button';
+import { useCart } from '../../hooks/useCart';
+
+const ProductDisplay = ({ product}) => {
+    const [selectedVariant, setSelectedVariant] = useState('');
+
+    const { addToCart} = useCart();
+
+    const handleAddToCart = () => {
+        if (!selectedVariant) {
+            alert('Please select a size first');
+            return;
+        }
+    }
+
+    const cartItem = {
+        ...product,
+        selectedVariant: selectedVariant
+    };
+
+    addToCart(cartItem);
+
+    const handleVariantSelect = (variant) => {
+        setSelectedVariant(variant);
+    };
+
+    return (
+        <div className="grid md:grid-cols-2 gap-8">
+            <ProductGallery images={product.images}/>
+
+            {/* Right side - Product info and controls */}
+            <div>
+                <h1 className="text-3xl font-bold mb-2">{product.name}</h1>
+
+                <p className="text-gray-600 mb-4">{product.description}</p>
+
+                <p className="text-2xl font-semibold text-blue-600 mb-6">
+                    ₱{product.price.toLocaleString()}
+                </p>
+
+                {/* */}
+            </div>
+        </div>
+    )
+};
+
+export default ProductDisplay;
---
+++ src/context/CartContext.js
@@ -0,0 +1,31 @@
+import { createContext, useState } from 'react';
+
+export const CartContext = createContext();
+
+export const CartProvider = ({children}) => {
+    const [cartItems, setCartItems] = useState([]);
+
+    const addToCart = (product) => {
+        console.log('Adding to cart', product);
+
+        setCartItems(previousItems => [...previousitems, product])
+
+        alert(`${product.name} (${product.selectedVariant} added to cart)`);
+    }
+
+    const getCartCount = () => {
+        return cartItems.length;
+    }
+
+    const contextValue = {
+        cartItems,
+        addToCart,
+        getCartCount
+    }
+
+    return (
+        <CartContext.Provider value={contextValue}>
+            {children}
+        </CartContext.Provider>
+    )
+}
